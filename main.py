2c688f7211ce3478283ee0384dccd59c681b93c1
Sandopaipa <kirill.kuli.01@gmail.com>
2023-01-27 00:49:24 +0300

    Dictionary for table data store is used

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
-
+# словарь - как одна строка таблицы
+# для хранения всей таблицы имеет смысл сделать словарь
 
 '''Парсер для конкретной HTML страницы, в случае, если нужно парсить
     страницу другого "формата" или вида, как будет угодно - нужно будет
@@ -7,14 +8,42 @@ from bs4 import BeautifulSoup
 
 time = []
 date = []
+buf = []
+#dict = {key: value} 19 столбцов таблицы
+dict = {
+    'time': None,
+    'date': None,
+    'wind_direction': None,
+    'wind_spd': None,
+    'visibility': None,
+    'cloudy': None,
+    't_c': None,
+    'td_c': None,
+    'f_percent': None,
+    'te_c': None,
+    'tes_c': None,
+    'comfort': None,
+    'p_gpa': None,
+    'po_gpa': None,
+    't_min_c': None,
+    't_max_c': None,
+    'r_mm': None,
+    'r24_mm': None,
+    's_cm': None,
 
+}
+
+
+'''Открываем документ для чтения'''
 with open('48300_dec.html', encoding='utf-8') as file:
     src = file.read()
 
 soup = BeautifulSoup(src, 'lxml')
+'''Парсим название таблицы (на всякий)'''
 title = soup.find("h1", class_="chronicle-title")
 title = title.contents
-print(title)
+#print(title)
+'''Парсим дату и время'''
 table_datetime = soup.find("div", class_="archive-table-left-column").find_all("td")
 for i in range(1, len(table_datetime)):
     if "." in str(table_datetime[i].contents):
@@ -28,4 +57,4 @@ table_main = soup.find("div", class_="archive-table-wrap").find_all("tr")
 for str in table_main:
     src = str.find_all("td")
     for cell in src:
-        print(cell.text.strip())
+        buf.append(cell.text.strip())
